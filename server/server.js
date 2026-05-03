23a24ce7f982b572bd420c62b01889534b29fa81
Asiya mohsima <asiyamohsima@gmail.com>
2026-05-03 21:29:44 +0530

    Added client folder

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -10,13 +12,16 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/crm")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 
 app.use("/api/leads", leadRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log("MONGO_URI VALUE:", process.env.MONGO_URI);
 });
